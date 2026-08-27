2d7adba5432638a38fa59e8b5c1f2ca04bc4aa10
Jérémie Rodon <jeremie.rodon@gmail.com>
2026-08-27 19:29:19 +0200

    wip

--- dist/nix/default.nix
+++ dist/nix/default.nix
@@ -10,6 +10,7 @@
 , onnxruntime
 , tpm2-tss
 , dbus
+, libxkbcommon
 }:
 
 rustPlatform.buildRustPackage {
@@ -37,6 +38,7 @@ rustPlatform.buildRustPackage {
     onnxruntime
     tpm2-tss
     dbus
+    libxkbcommon
   ];
 
   # Tests require camera hardware
